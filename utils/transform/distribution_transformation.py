0adf398c68b0c87182695b4054c2043a7481d538
chenziao <zc963@mail.missouri.edu>
2021-09-03 16:57:10 -0500

    update transform functions

--- utils/transform/distribution_transformation.py
+++ utils/transform/distribution_transformation.py
@@ -13,3 +13,22 @@ def range2logn(a,b,n_sigma=2):
     mu = (A+B)/2
     sigma = (B-A)/n_sigma/2
     return mu, sigma
+
+def norm2logn(x,mu,sigma):
+	"""Transform normal distributed variables to follow lognormal (mu,sigma) distribution"""
+	y = np.exp(mu+sigma*x)
+	return y
+
+
+# 'logds' stands for log probability density offset by variable transformation
+# E.g. if variable x has probability density f(x) and variable y=g(x),
+# then y has probatility density f(x)*|dx/dy|, and log density log(f(x))+log(|dx/dy|)
+# logds calculates log(|dx/dy|).
+
+def logds_norm2unif(x,a,b):
+	logds = -np.log(b-a)-np.log(norm.pdf(x))
+	return logds
+
+def logds_norm2logn(x,mu,sigma):
+	logds = -np.log(sigma)-mu-sigma*x
+	return logds
