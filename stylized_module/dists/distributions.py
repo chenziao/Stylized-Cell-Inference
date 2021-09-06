72c569b4991f5c81b9432acb416996e4bd7dc340
Matthew Carroll <matthew.j.carroll@outlook.com>
2021-09-06 16:05:32 -0500

    working on broadcast shape

--- stylized_module/dists/distributions.py
+++ stylized_module/dists/distributions.py
@@ -4,20 +4,20 @@ from torch.distributions import Normal, Uniform
 from pyro.distributions.torch_distribution import TorchDistribution
 from pyro.distributions.util import broadcast_shape
 
-import config.params as params
 
 class StackedDistribution(TorchDistribution):
     def __init__(
         self,
-        *base_dists,
-        validate_args=True
+        base_dists,
+        validate_args=False
     ):
-        assert len(set(base_dist.event_shape for base_dist in base_dists)) == 1 \
+        assert len(set(base_dist.event_shape for base_dist in base_dists)) == 1, \
             "All base distributions should have the same event shape"
-        self.batch_shape = broadcast_shape(*[base_dist.batch_shape for base_dist in base_dists])
-        self.event_shape = base_dists[0].event_shape + (len(base_dists),)
-        self.base_dists = tuple(base_dist.expand(batch_shape) for base_dist in base_dists)
-        super().__init__(self.batch_shape, self.event_shape, validate_args)
+        batch_shape = broadcast_shape([base_dist.batch_shape for base_dist in base_dists])
+        bs = [1 for i in batch_shape] #done because each distribution is 1D and torch.size flattens this
+        event_shape = base_dists[0].event_shape + (len(base_dists),)
+        self.base_dists = tuple(base_dist.expand(bs) for base_dist in base_dists)
+        super().__init__(batch_shape, event_shape, validate_args)
     
     def log_prob(self, x):
         return sum(base_dist.log_prob(x[..., i]) for i, base_dist in enumerate(self.base_dists))
@@ -33,7 +33,7 @@ class StackedDistribution(TorchDistribution):
         return all(base_dist.has_rsample for base_dist in self.base_dists)
 
 
-def build_priors(bounds: dict[str, float] = params.IM_PARAMETER_DICT):
+def build_priors(bounds):
     priors = []
     for k, v in bounds:
         if k == 'U':
