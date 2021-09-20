381d3f7f66a9682bbc7542e9418b8d31f02dd32d
Matthew J Carroll <mjc6r9@engr-nelvm-res.engineering.missouri.edu>
2021-09-20 13:02:21 -0500

    run with 2D electrode fixed

--- stylized_module/models/cnn.py
+++ stylized_module/models/cnn.py
@@ -47,7 +47,7 @@ class SummaryNet(nn.Module):
 
 
 class SummaryNet3D(nn.Module):
-    def __init__(self, nelec, window_size, nstats=24):
+    def __init__(self, nelec, window_size, nstats=31):
         super(SummaryNet3D, self).__init__()
         
         self.nelec = nelec
@@ -55,7 +55,7 @@ class SummaryNet3D(nn.Module):
         self.nout = window_size*self.nelec
         self.nstats = nstats
         
-        # 96x4x96 -> 96x4x96
+        # 96x4x96 -> 96x4x190
         self.conv1 = nn.Conv3d(in_channels=1, out_channels=5, kernel_size=3, padding=1, padding_mode='replicate')
         # 96x4x96 -> 48x2x48
         self.pool1 = nn.MaxPool3d(kernel_size=2)
@@ -72,21 +72,28 @@ class SummaryNet3D(nn.Module):
         # 10x10 -> 5x5
         self.pool4 = nn.MaxPool2d(kernel_size=2)
         # Fully connected layer taking as input the 8 flattened output arrays from the maxpooling layer
-        self.fc = nn.Linear(in_features=125, out_features=24) # 5*5*5=125
-        self.fc2 = nn.Linear(in_features=24+self.nstats, out_features=12)
+        self.fc = nn.Linear(in_features=250, out_features=50) # 5*5*5=125
+        self.fc2 = nn.Linear(in_features=50+self.nstats, out_features=12)
 
     def forward(self,x):
-        x0 = x[:,self.nout:] # n x 24
-        x = x[:,:self.nout] # n x 36864
-        x = x.view(-1,1,96,4,96)
+#         print(x.shape)
+        x0 = x[:,(-self.nstats):] # n x 24
+        x = x[:,:(-self.nstats)] # n x 36864
+        x = x.view(-1,1,96,4,190)
         #x = x.view(-1,1,self.window_size,4,self.nelec) # (batch size,in_channels,height,width,length) -1 means not changing size of that dimension
         x = self.pool1(F.relu(self.conv1(x)))
+#         print(x.shape)
         x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(-1,5,24,24)
+#         print(x.shape)
+        x = x.view(-1,5,24,47)
         x = self.pool3(F.relu(self.conv3(x)))
+#         print(x.shape)
         x = self.pool4(F.relu(self.conv4(x)))
-        x = x.view(-1,125) # (batch size, in_features)
+#         print(x.shape)
+        x = x.view(-1,250) # (batch size, in_features)
         x = self.fc(x)
+#         print(x.shape)
+#         print(x0.shape)
         x = torch.cat((x,x0),dim=1)
         # x = self.fc2(x)
         return x
