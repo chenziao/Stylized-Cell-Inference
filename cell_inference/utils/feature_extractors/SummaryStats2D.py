2cd6b04ebae1933089e783a62e62bfa3a2c62429
chenziao <chen8a803@gmail.com>
2022-01-27 01:04:36 -0600

    Add get y window functions

--- cell_inference/utils/feature_extractors/SummaryStats2D.py
+++ cell_inference/utils/feature_extractors/SummaryStats2D.py
@@ -9,11 +9,38 @@ import cell_inference.config.params as params
 GRID = params.ELECTRODE_GRID
 GRID_SHAPE = tuple(v.size for v in GRID)
 
+def get_y_window(lfp: np.ndarray, coord: np.ndarray,
+                 y_window_size: float = 960.,
+                 grid_v: Union[np.ndarray, Tuple[np.ndarray, np.ndarray, np.ndarray]] = None
+                 ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Get a window along y-axis centered at the maximum amplitude location.
+        lfp: LFP array with shape (time x channels)
+        coord: Coordinates of each channel (channels x 2)
+        y_window_size: The width of the window (micron)
+        grid_v: The grid vectors we use to get the window index. (x grid, y grid, z grid)
+            If not specified, use params.ELECTRODE_GRID.
+    Return Windowed grid y coordinates, Index of the window in the grid
+    """
+    grid_y = grid_v[1]
+    y_size = grid_y.size
+    # relative index of window in y grid
+    dy = abs(grid_y[-1] - grid_y[0]) / (y_size - 1)
+    ny = max(int(np.floor(y_window_size / 2 / dy) * 2) + 1, 3)
+    rel_idx = np.arange(-(ny - 1) / 2,(ny + 1) / 2, dtype=int)
+    # find maximum amplitude location
+    max_idx = np.argmax(np.amax(np.abs(lfp), axis=0))
+    center_y = coord[max_idx, 1]
+    center_y_idx = np.argmin(np.abs(grid_y - center_y))
+    if center_y_idx + rel_idx[0] < 0 or center_y_idx + rel_idx[-1] > y_size:
+        raise ValueError("The window falls outside the given electrode grid range.")
+    y_window_idx = center_y_idx + rel_idx
+    grid_y = grid_y[y_window_idx]
+    return grid_y, y_window_idx
 
-def build_lfp_grid(lfp: np.ndarray,
-                   coord: np.ndarray,
-                   grid_v: Optional[Union[np.ndarray, Tuple[np.ndarray, np.ndarray, np.ndarray]]] = None,
-                   y_window_size: Optional[Union[float, int, np.ndarray]] = None
+def build_lfp_grid(lfp: np.ndarray, coord: np.ndarray,
+                   y_window_size: Optional[float] = None,
+                   grid_v: Optional[Union[np.ndarray, Tuple[np.ndarray, np.ndarray, np.ndarray]]] = None
                    ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Build a grid to match the Neuropixel Probe used to collect
@@ -21,37 +48,52 @@ def build_lfp_grid(lfp: np.ndarray,
     
     Then interpolate the LFP onto the grid for each timestep
         y_window_size - If specified, get the grid within a window along y-axis instead,
-        with the window centered at the maximum amplitude location. (micron)
-        Return None if the window falls outside the given electrode array range.
+            with the window centered at the maximum amplitude location. (micron)
+        Raise error if the window falls outside the given electrode array range.
+    Return Gridded LFP array with shape (time x channels), Grid coordinates (channels x 2)
     """
     if grid_v is None:
         grid_v = GRID
-    t = lfp.shape[0]
-    xy = coord[:, :2]
-    grid_y = grid_v[1]
-    if y_window_size is not None:
-        # relative index of window in y grid
-        dy = abs(grid_y[-1] - grid_y[0]) / (GRID_SHAPE[1] - 1)
-        ny = max(int(np.floor(y_window_size/dy)) + 1, 3)
-        rel_idx = int(np.floor((ny - 1) / 2)) + np.arange(ny, dtype=int)
-        # find maximum amplitude location
-        max_idx = np.argmax(np.amax(np.abs(lfp), axis=0))
-        center_y = xy[max_idx, 1]
-        center_y_idx = np.argmin(np.abs(grid_y - center_y))
-        if center_y_idx + rel_idx[0] < 0 or center_y_idx + rel_idx[-1] > GRID_SHAPE[1]:
-            raise ValueError("The window falls outside the given electrode array range.")
-        else:
-            grid_y = grid_y[center_y_idx + rel_idx]
+    if y_window_size is None:
+        grid_y = grid_v[1]
+    else:
+        grid_y, _ = get_y_window(lfp=lfp, coord=coord, y_window_size=y_window_size, grid_v=grid_v)
     xx, yy = np.meshgrid(grid_v[0], grid_y, indexing='ij')
     grid = np.column_stack((xx.ravel(), yy.ravel()))
+    t = lfp.shape[0]
     grid_lfp = np.empty((t, grid.shape[0]))
     for i in range(t):
-        grid_lfp[i, :] = griddata(xy, lfp[i, :], grid)
+        grid_lfp[i, :] = griddata(coord[:, :2], lfp[i, :], grid)
     return grid_lfp, grid
 
+def get_lfp_y_window(g_lfp: np.ndarray, coord: np.ndarray,
+                     y_window_size: float = 960.,
+                     grid_v: Optional[Union[np.ndarray, Tuple[np.ndarray, np.ndarray, np.ndarray]]] = None
+                     ) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Get LFP within a window along y-axis centered at the maximum amplitude location.
+        g_lfp: Gridded LFP array with shape (time x channels)
+        grid_v: The grid vectors we use to get the window index. (x grid, y grid, z grid)
+            If not specified, use params.ELECTRODE_GRID.
+            Raise error if grid is not consistent with g_lfp channel dimensions.
+    Return LFP array within y window with shape (time x channels)
+    """
+    if grid_v is None:
+        grid_v = GRID
+        grid_shape = GRID_SHAPE
+    else:
+        grid_shape = tuple(v.size for v in grid_v)
+    if g_lfp.shape[1] != grid_shape[0]*grid_shape[1]:
+        raise ValueError("LFP array is not consistent with grid shape.")
+    _, y_window_idx = get_y_window(lfp=g_lfp, coord=coord, y_window_size=y_window_size, grid_v=grid_v)
+    t = g_lfp.shape[0]
+    lfp_y_window = g_lfp.reshape((t, grid_shape[0], grid_shape[1]))[:,:,y_window_idx].reshape((t, -1))
+    return lfp_y_window
 
-def calculate_stats(g_lfp: np.ndarray,
-                    additional_stats: bool = True) -> np.ndarray:
+
+def calculate_stats(g_lfp: np.ndarray, additional_stats: bool = True,
+                    grid_shape: Optional[Tuple[int]] = None
+                    ) -> np.ndarray:
     """
     Calculates summary statistics. This includes:
         - Average Voltage of Each Channel
@@ -60,9 +102,17 @@ def calculate_stats(g_lfp: np.ndarray,
         - peaks of Each Channel
         - Difference between Peak and Trough time
         - Width of Waveform from half height (if additional_stats True)
+        g_lfp: gridded LFP array with shape (time x channels)
+        grid_shape: grid shape of g_lfp.
+            If not specified, use x size in params.ELECTRODE_GRID and infer y size automatically.
     """
     g_lfp = np.asarray(g_lfp)
-    grid_shape = (GRID_SHAPE[0], int(g_lfp.shape[1]/GRID_SHAPE[0]))
+    if grid_shape is None:
+        grid_shape = (GRID_SHAPE[0], int(g_lfp.shape[1]/GRID_SHAPE[0]))
+    else:
+        grid_shape = grid_shape[:2]
+    if g_lfp.shape[1] != grid_shape[0]*grid_shape[1]:
+        raise ValueError("LFP array is not consistent with grid shape.")
     avg = np.mean(g_lfp, axis=0)  # average voltage of each channel
     std_dev = np.std(g_lfp, axis=0)  # stDev of the voltage of each channel
     t_t = np.argmin(g_lfp, axis=0)
@@ -165,8 +215,7 @@ def calculate_stats(g_lfp: np.ndarray,
     return all_stats
 
 
-def cat_output(lfp: np.ndarray,
-               include_sumstats=True) -> torch.Tensor:
+def cat_output(lfp: np.ndarray, include_sumstats=True) -> torch.Tensor:
     """
     Driver function. Creates grid and interpolates provided LFP then concatenates
     calculated summary statistics to the interpolated LFP
