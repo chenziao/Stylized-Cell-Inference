815e23312a0b821b538007dcd35090662358ccb5
chenziao <chen8a803@gmail.com>
2022-07-20 23:42:09 -0500

    Update data simulation. Update fully connected network architecture

--- cell_inference/utils/feature_extractors/fullyconnectednetwork.py
+++ cell_inference/utils/feature_extractors/fullyconnectednetwork.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from typing import Optional
+from typing import Optional, Union, List
 from collections import OrderedDict
 from enum import Enum
 
@@ -15,41 +15,35 @@ class ActivationTypes(Enum):
 class FullyConnectedNetwork(nn.Module):
 
     def __init__(self, in_features: int, out_features: int,
-                 hidden_layers: int = 2, hidden_layer_size: int = 50,
+                 hidden_layers: int = 2, hidden_layer_size: Union[int,List[int]] = None,
                  activation: Optional[ActivationTypes] = None) -> None:
 
-        self.__activation_mapping = {ActivationTypes.ReLU: nn.ReLU(),
-                                     ActivationTypes.TanH: nn.Tanh(),
-                                     ActivationTypes.Sigmoid: nn.Sigmoid(),
-                                     ActivationTypes.LeakyReLU: nn.LeakyReLU(),
-                                     None: nn.ReLU()}
+        self.__activation_mapping = {ActivationTypes.ReLU: nn.ReLU,
+                                     ActivationTypes.TanH: nn.Tanh,
+                                     ActivationTypes.Sigmoid: nn.Sigmoid,
+                                     ActivationTypes.LeakyReLU: nn.LeakyReLU,
+                                     None: nn.ReLU}
 
         super(FullyConnectedNetwork, self).__init__()
+        if hidden_layer_size is None:
+            hidden_layer_size = [64, 124, 64, 32, 16]
+        if hasattr(hidden_layer_size, '__len__'):
+            hidden_layers = len(hidden_layer_size)
+        else:
+            hidden_layer_size = [hidden_layer_size] * hidden_layers
+        hidden_layer_size = [in_features] + list(hidden_layer_size)
         self.activation = self.__activation_mapping[activation]
-        self.input_layer = nn.Linear(in_features=in_features, out_features=hidden_layer_size)
+
         layers = OrderedDict()
-        for i in range(hidden_layers * 2):
-            layers[str(i)] = nn.Linear(hidden_layer_size, hidden_layer_size) if i % 2 == 0 else self.activation
+        for i in range(hidden_layers):
+            layers[str(i*2)] = nn.Linear(in_features=hidden_layer_size[i], out_features=hidden_layer_size[i+1])
+            layers[str(i*2+1)] = self.activation()
+        self.hidden_layers = nn.Sequential(layers)
 
-        # self.hidden_layers = nn.Sequential(layers)
+        self.output_layers = nn.Linear(in_features=hidden_layer_size[-1], out_features=out_features)
 
-        self.hidden_layers = nn.Sequential(
-            nn.Linear(hidden_layer_size, 100),
-            nn.ReLU(),
-            nn.Linear(100, 124),
-            nn.ReLU(),
-            nn.Linear(124, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 10),
-            nn.ReLU()
-        )
-
-        self.output_layers = nn.Linear(in_features=10, out_features=out_features)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.activation(self.input_layer(x))
         x = self.hidden_layers(x)
         x = self.output_layers(x)
         return x
