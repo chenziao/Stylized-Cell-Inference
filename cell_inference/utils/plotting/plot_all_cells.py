7be0d0fd31c4216eb665e910db077dafa0e9dc55
chenziao <chen8a803@gmail.com>
2022-10-13 22:12:18 -0500

    Update prediction visualization

--- cell_inference/utils/plotting/plot_all_cells.py
+++ cell_inference/utils/plotting/plot_all_cells.py
@@ -11,8 +11,7 @@ from cell_inference.utils.transform.geometry_transformation import pol2cart, hph
 
 
 def plot_all_cells(df: pd.DataFrame, s: float = 10, w: float = 2,
-                   axes=None,
-                   view: Union[str, List[float], Tuple[float, float]] = '3D',
+                   axes=None, view: Union[str, List[float], Tuple[float, float]] = '3D',
                    figsize: Optional[Tuple[float, float]] = None) -> Tuple[Figure, Axes]:
     """
     Plot multiple cells location, orientation and simplified morphology in 3D,
@@ -28,7 +27,8 @@ def plot_all_cells(df: pd.DataFrame, s: float = 10, w: float = 2,
     """
     if axes is None:
         axes = [2, 0, 1]
-    if not {'y', 'h', 'phi', 'r_s', 'l_t', 'r_t'}.issubset(set(df.columns)):
+    df = df.copy()
+    if not {'y', 'h', 'phi'}.issubset(set(df.columns)):
         raise ValueError("Lacking features of position or morphology.")
 
     if 'x' not in df.columns or 'z' not in df.columns:
@@ -36,25 +36,34 @@ def plot_all_cells(df: pd.DataFrame, s: float = 10, w: float = 2,
             raise ValueError("Lacking features of position or morphology.")
         else:
             df['x'], df['z'] = pol2cart(df['d'], df['theta'])
+    if 'r_s' in df.columns:
+        r_s = s / df['r_s'].mean() * df['r_s'].values
+    else:
+        r_s = np.full(len(df), s)
+    if 'r_t' in df.columns:
+        r_t = w / df['r_t'].mean() * df['r_t'].values
+    else:
+        r_t = np.full(len(df), w)
+    if 'l_t' not in df.columns:
+        df['l_t'] = 300.
 
     p0 = df[['x', 'y', 'z']].values
     p1 = p0 + hphi2unitsphere(df[['h', 'phi']].values) * df[['l_t']].values
-
-    r_s = s / df['r_s'].mean() * df['r_s'].values
-    r_t = w / df['r_t'].mean() * df['r_t'].values
 
     sm = plt.cm.ScalarMappable(cmap=cm.viridis, norm=plt.Normalize())
     sm.set_array(r_t)
     sm.autoscale()
 
     fig = plt.figure(figsize=figsize)
-    ax = plt.axes(projection='3d')
+    ax = fig.add_subplot(projection='3d')
 
-    somas = ax.scatter(*p0[:, axes].T, c=r_s, s=r_s, cmap='plasma')
+    somas = ax.scatter(*p0.T[axes], c=r_s, s=r_s, cmap='plasma')
     for i in range(len(df)):
-        trunks = ax.plot3D(*np.vstack((p0[i], p1[i]))[:, axes].T, color=sm.to_rgba(r_t[i]), linewidth=r_t[i])
-    fig.colorbar(sm, fraction=0.1, shrink=0.5, aspect=30, label='trunk radius (um)')
-    fig.colorbar(somas, fraction=0.1, shrink=0.5, aspect=30, label='soma radius (um)')
+        trunks = ax.plot3D(*np.vstack((p0[i], p1[i])).T[axes], color=sm.to_rgba(r_t[i]), linewidth=r_t[i])
+    if 'r_t' in df.columns:
+        fig.colorbar(sm, fraction=0.1, shrink=0.5, aspect=30, label='trunk radius')
+    if 'r_s' in df.columns:
+        fig.colorbar(somas, fraction=0.1, shrink=0.5, aspect=30, label='soma radius')
 
     xyz = 'xyz'
     ax.set_xlabel(xyz[axes[0]] + ' (um)')
@@ -64,16 +73,14 @@ def plot_all_cells(df: pd.DataFrame, s: float = 10, w: float = 2,
     box = np.vstack((np.vstack((p0, p1)).min(axis=0), np.vstack((p0, p1)).max(axis=0)))
     center = box.mean(axis=0)
     box = center + (box[1] - box[0]).max() / 2 * np.array([[-1], [1]])
-    # ax.auto_scale_xyz(*box.T[axes])
-    # ax.set_xlim(box.T[axes[0]])
-    # ax.set_ylim(box.T[axes[1]])
-    # ax.set_zlim(box.T[axes[2]])
+    ax.set_xlim(box.T[axes[0]])
+    ax.set_ylim(box.T[axes[1]])
+    ax.set_zlim(box.T[axes[2]])
 
     if type(view) is str:
         if view == '2D':
-            ax.view_init(elev=0, azim=0)
+            ax.view_init(elev=0, azim=-90)
     else:
         ax.view_init(elev=view[0], azim=view[1])
 
-    plt.show()
     return fig, ax
