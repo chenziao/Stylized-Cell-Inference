17a1b20e219a71c73b0d944bb0a12dcd1db8ef55
chenziao <chen8a803@gmail.com>
2022-11-04 23:53:17 -0500

    Add full range h prediction results; Plot predictions with cutoff correlation coefficient

--- cell_inference/utils/transform/data_transform.py
+++ cell_inference/utils/transform/data_transform.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def log_modulus(x, x0=1):
+def log_modulus(x, x0=1.):
     """
     Log modulus transfomr
     x0: reference magnitude (x0 > 0), values beyond which get spreaded out. x0 |-> 1, x0 maps to unit value.
@@ -9,3 +8,7 @@ def log_modulus(x, x0=1):
     if x0<=0:
         raise ValueError("Parameter x0 must be positive.")
     return np.sign(x) * (np.log2(np.abs(x) + x0) - np.log2(x0))
+
+def log_moulus_nfold(x, n_fold=31.):
+    """Apply transform with reference value being 1/n_fold of the maximum magnitude"""
+    return log_modulus(x, np.amax(np.abs(x)) / n_fold)
